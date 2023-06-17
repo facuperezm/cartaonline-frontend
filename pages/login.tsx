e45093194bb89dd9d840c67a3693f92d804b6097
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2023-06-17 16:39:12 -0300

    feat: :tada: working on auth

---
+++ pages/login.tsx
@@ -0,0 +1,90 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { env } from "@/env.mjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.coerce.string().email().min(5, {
+    message: "email must be at least 2 characters.",
+  }),
+  password: z.string().min(3, {
+    message: "Password must be at least 8 characters.",
+  }),
+});
+
+export default function Login() {
+  const formData = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const res = axios.post(
+      env.NEXT_PUBLIC_BACKEND_BASE_URL + "/api/users/auth",
+      values
+    );
+    console.log(res);
+  }
+
+  return (
+    <>
+      <Form {...formData}>
+        <form onSubmit={formData.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={formData.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="admin@gmail.com" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={formData.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="****************"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Password must be at least 8 characters.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
+}
